c5c154cf474f82c4e66be411b5bab528f56a5c61
Nhantp <phucnhant@gmail.com>
2022-06-29 15:30:55 +0700

    nopbai

---
+++ ss11_cau_truc_lap_2/bai_tap/bai8.js
@@ -0,0 +1,36 @@
+function number() {
+    let nhap1 = parseInt(prompt("Nhập vào khoảng thứ 1:"));
+    let nhap2 = parseInt(prompt("Nhập vào khoảng thứ 2:"));
+    let nhap3 = parseInt(prompt("Nhập vào số dự đoán"));
+    let nhap4;
+    let re1 = Math.floor(Math.random() * nhap2) + nhap1;
+    count = 0;
+    while (count <= 3) {
+        if (nhap3 === re1) {
+            alert("Chúc mừng du đã đúng \n Phần thưởng là cái nịt.");
+            break;
+        } else {
+            alert("Sai rồi bạn ê.")
+            nhap4 = parseInt(prompt("Đoán lại đi bạn ê."))
+            count++;
+            if (nhap4 === re1) {
+                alert("Chúc mừng du đã đúng \n Phần thưởng là cái nịt.")
+                break;
+            } else {
+                alert("Sai rồi bạn ê.")
+                nhap4 = parseInt(prompt("Đoán lại đi bạn ê."))
+                count++;
+            }
+            if (nhap4 === re1) {
+                alert("Chúc mừng du đã đúng \n Phần thưởng là cái nịt.")
+                break;
+            } else {
+                alert("Sai rồi bạn ê.")
+                nhap4 = parseInt(prompt("Đoán lại đi bạn ê."))
+                count++;
+            }
+        }
+        count++;
+        alert("Vui lòng nạp vào lần đầu để có thêm lượt");
+    }
+}

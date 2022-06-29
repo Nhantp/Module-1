12289e05b62b11b4e99a6b6a45e1a5649235829e
Nhantp <phucnhant@gmail.com>
2022-06-29 16:02:20 +0700

    nopbai

--- ss11_cau_truc_lap_2/bai_tap/bai8.js
+++ ss11_cau_truc_lap_2/bai_tap/bai8.js
@@ -5,7 +5,7 @@ function number() {
     let nhap4;
     let re1 = Math.floor(Math.random() * nhap2) + nhap1;
     count = 0;
-    while (count <= 3) {
+    while (count < 3) {
         if (nhap3 === re1) {
             alert("Chúc mừng du đã đúng \n Phần thưởng là cái nịt.");
             break;
@@ -21,14 +21,7 @@ function number() {
                 nhap4 = parseInt(prompt("Đoán lại đi bạn ê."))
                 count++;
             }
-            if (nhap4 === re1) {
-                alert("Chúc mừng du đã đúng \n Phần thưởng là cái nịt.")
-                break;
-            } else {
-                alert("Sai rồi bạn ê.")
-                nhap4 = parseInt(prompt("Đoán lại đi bạn ê."))
                 count++;
-            }
         }
         count++;
         alert("Vui lòng nạp vào lần đầu để có thêm lượt");
